ee239706996aafadeed8134a35cf24be31bb4351
ErickG <erick818@gmail.com>
2021-10-27 15:19:32 -0700

    first commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,44 @@
+import "./About.css";
+import Award from "../../img/bootstarp.png";
+import Award2 from "../../img/award.jpg"
+
+const About = () => {
+    return (
+        <div className="a">
+            <div className="a-left">
+                <div className="a-card bg"></div>
+                <div className="a-card">
+                    <img
+                        src={Award2}
+                        alt=""
+                        className="a-img"
+                    />
+                </div>
+            </div>
+            <div className="a-right">
+                <h1 className="a-title">About Me</h1>
+                <p className="a-sub">
+                    It is a long established fact that a reader will be distracted by the
+                    readable content.
+                </p>
+                <p className="a-desc">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
+                </p>
+                <div className="a-award">
+                    <img src={Award} alt="" className="a-award-img" />
+                    <div className="a-award-texts">
+                        <h4 className="a-award-title">Front-End Web Development:Bootstrap 2021</h4>
+                        <p className="a-award-desc">
+                            Full stack web and Mobile App. 5 Week Bootstarp Completion
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
