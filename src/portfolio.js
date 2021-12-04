cd565bb57a29e51edd59707a29157634372f8e38
ErickG <erick818@gmail.com>
2021-12-03 21:30:18 -0800

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,16 +1,16 @@
 const projects = [
     {
-        name: 'Nucamp Website With BootStrap',
+        name: 'Nucamp Website Using BootStrap',
         description:
-            'On this project, we took all the basic HTML, CSS, and Javascript and implemented Bootstrap. Bootstrap is a CSS framework that allows us to develop responsive websites using the mobile-first method. This method ensures that your website will work responsively on any screen size such as an iPhone or a desktop. This makes the user experience enjoyable on any device and every time. ',
-        stack: ['HTML', 'CSS', 'JavaScrip', 'Bootsrap'],
+            'HTML, CSS, and Javascript are three components used to build a simple desktop-style website. But what if we want the user to have access on the go. For this website, I  implemented Bootstrap. What is Bootstrap? Bootstrap is a CSS framework that allows us to develop responsive websites using the mobile-first method. This method ensures that your website will work responsively on any screen size like an iPhone or even your desktop. Providing an enjoyable experience for the user no matter what device they are on. ',
+        stack: ['HTML', 'CSS', 'JavaScrip', 'Bootstrap'],
         sourceCode: 'https://github.com/CrypticNightOwl/nucamp',
         livePreview: 'https://crypticnightowl.github.io/nucamp/',
     },
     {
-        name: 'Nucamp Website With React',
+        name: 'Nucamp Website Using React',
         description:
-            'This website is very similar to the Bootstrap version except here we are only using JavaScript and React.',
+            'This project is still under construction. Please feel free to take a look at the source code. While this is still being developed.',
         stack: ['HTML', 'CSS', 'JavaScript', 'React'],
         sourceCode: 'https://github.com/CrypticNightOwl/nucampsiteReact',
         livePreview: 'https://github.com',
